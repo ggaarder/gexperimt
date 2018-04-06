92a355b7b287ac9292cc2aabdcb14b512fd645dc
George Gaarder <ggaarder@foxmail.com>
2018-04-06 11:01:12 +0800

    add comments

--- mmap.c
+++ mmap.c
@@ -6,10 +6,10 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-void test(char *fn) {
+void test(char *fn) { // reading
   int fd = open(fn, O_RDONLY);
   int len = lseek(fd, 0, SEEK_END)+1;
-  //lseek(fd, 0, SEEK_SET);
+  // there is no need to seek back:  lseek(fd, 0, SEEK_SET);
   printf("%s len: %d\n", fn, len);
   char *txt = (char*)mmap(NULL, len, PROT_READ, MAP_PRIVATE, fd, 0);
   char *p;
@@ -22,7 +22,7 @@ int main() {
   test("mmap.c");
   test("train-labels.idx1-ubyte");
 
-  int fd = open("foo", O_RDWR);
+  int fd = open("foo", O_RDWR); // writing
   int len = lseek(fd, 0, SEEK_END)+1;
   char *raw = (char*)mmap(NULL, len, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
   srand(time(0));
@@ -30,7 +30,7 @@ int main() {
   munmap(raw, len);
   close(fd);
 
-  fd = open("bar", O_RDWR);
+  fd = open("bar", O_RDWR); // writing with MAP_PRIVATE? you can't.
   len = lseek(fd, 0, SEEK_END)+1;
   raw = (char*)mmap(NULL, len, PROT_READ|PROT_WRITE, MAP_PRIVATE, fd, 0);
   srand(time(0));
